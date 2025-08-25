601f5537112c3070b7580e55fa660ed35ae7ce1e
Arman Gökalp <armangokalp@gmail.com>
2025-08-25 16:09:43 +0300

    initial
    abc

---
+++ Movies Catalog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies Catalog
+//
+//  Created by Arman Gökalp on 25.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
